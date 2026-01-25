f4c802ae72ca3867fb9a1d802873578c332ffd32
att291K <att291@mail.ru>
2026-01-25 13:16:15 +0300

    начало

---
+++ src/test/java/ru/edu/taskmanagemntsystem/TaskManagemntSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.edu.taskmanagemntsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagemntSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
