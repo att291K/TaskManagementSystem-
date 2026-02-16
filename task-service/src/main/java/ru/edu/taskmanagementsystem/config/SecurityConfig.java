90ca7ce9d445371b4b173cf0bdc6ad13b511f7a8
att291K <att291@mail.ru>
2026-02-16 22:13:53 +0300

    last change

--- task-service/src/main/java/ru/edu/taskmanagementsystem/config/SecurityConfig.java
+++ task-service/src/main/java/ru/edu/taskmanagementsystem/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
         return config.getAuthenticationManager();
          }
 }
